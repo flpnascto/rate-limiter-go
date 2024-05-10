4d7a83b604854f1b85da3148c6866b15ac40279f
Felipe Nascimento <flpnascto@gmail.com>
2024-05-09 23:12:53 -0300

    feat: create rate limirt entity

---
+++ internal/entity/rate-limiter.go
@@ -0,0 +1,82 @@
+package entity
+
+import (
+	"errors"
+	"sync"
+	"time"
+)
+
+type Visitor struct {
+	identity string
+	requests int
+	created  time.Time
+}
+
+type RateLimiter struct {
+	visitors    []Visitor
+	maxRequests int
+	mu          sync.Mutex
+}
+
+func NewRateLimiter(limit int) *RateLimiter {
+	r := &RateLimiter{
+		visitors:    []Visitor{},
+		maxRequests: limit,
+	}
+
+	go r.cleanup()
+
+	return r
+}
+
+func (r *RateLimiter) AddIpVisitor(ip string) error {
+	index := -1
+	for i, v := range r.visitors {
+		if v.identity == ip {
+			index = i
+			break
+		}
+	}
+	if index == -1 {
+		r.visitors = append(r.visitors, newVisitor(ip))
+	} else {
+		r.visitors[index].requests++
+		r.visitors[index].created = time.Now()
+		err := r.limit(r.visitors[index])
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func newVisitor(ip string) Visitor {
+	return Visitor{
+		identity: ip,
+		requests: 1,
+		created:  time.Now(),
+	}
+}
+
+func (r *RateLimiter) limit(v Visitor) error {
+	if v.requests > r.maxRequests {
+		return errors.New("too many requests")
+	}
+	return nil
+}
+
+func (r *RateLimiter) cleanup() {
+	for {
+		time.Sleep(100 * time.Millisecond)
+
+		r.mu.Lock()
+		newVisitors := make([]Visitor, 0)
+		for _, y := range r.visitors {
+			if time.Since(y.created) <= 1*time.Second {
+				newVisitors = append(newVisitors, y)
+			}
+		}
+		r.visitors = newVisitors
+		r.mu.Unlock()
+	}
+}
