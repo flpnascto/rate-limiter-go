ad49e748f300fd4c5ffcff1469485972bf7f993d
Felipe Nascimento <flpnascto@gmail.com>
2024-05-13 17:39:02 -0300

    feat: distinct request with token

--- internal/infra/web/rate-limiter.middleware.go
+++ internal/infra/web/rate-limiter.middleware.go
@@ -10,21 +10,47 @@ import (
 
 func RateLimiterMiddleware(next http.Handler) http.Handler {
 	ipRateLimiter := entity.NewRateLimiter(5)
+	tokenRateLimiter := entity.NewRateLimiter(6)
+	var message string
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		ip := r.RemoteAddr
-		if ip != "" {
-			host, _, err := net.SplitHostPort(ip)
-			if err != nil {
-				log.Fatal(err)
-			}
-			ip = host
+		if apiKey := r.Header.Get("API_KEY"); apiKey != "" {
+			message = requestWithToken(r, tokenRateLimiter)
+		} else {
+			message = requestWithIp(r, ipRateLimiter)
 		}
-		err := ipRateLimiter.AddIpVisitor(ip)
-		if err != nil {
-			message := "too many requests from " + ip
+		if message != "" {
 			http.Error(w, message, http.StatusTooManyRequests)
 			return
 		}
 		next.ServeHTTP(w, r)
 	})
+}
+
+func requestWithToken(r *http.Request, ipRateLimiter *entity.RateLimiter) string {
+	token := r.Header.Get("API_KEY")
+	err := ipRateLimiter.AddIpVisitor(token)
+	if err != nil {
+		//TODO: change message for: "you have reached the maximum number of requests or actions allowed within a certain time frame"
+		message := "too many requests with token " + token
+		return message
+	}
+	return ""
+}
+
+func requestWithIp(r *http.Request, tokenRateLimiter *entity.RateLimiter) string {
+	ip := r.RemoteAddr
+	if ip != "" {
+		host, _, err := net.SplitHostPort(ip)
+		if err != nil {
+			log.Fatal(err)
+		}
+		ip = host
+	}
+	err := tokenRateLimiter.AddIpVisitor(ip)
+	if err != nil {
+		//TODO: change message for: "you have reached the maximum number of requests or actions allowed within a certain time frame"
+		message := "too many requests from " + ip
+		return message
+	}
+	return ""
 }
